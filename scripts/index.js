4f4c43fa1c807fe601afea3c808eabd128e944c9
JabedH <jabedhtusar@gmail.com>
2022-09-29 01:58:02 +0600

    added slider

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
-const allClass = ["banner-1", "banner-2", "banner-3"];
+const allClass = ["banner-2", "banner-3", "banner-1"];
 const getClass = document.getElementById("main-slider");
+const setClass = document
+  .getElementById("main-slider")
+  .setAttribute("class", "banner-1");
 let classIndex = 0;
 setInterval(() => {
   if (classIndex >= allClass.length) {
@@ -7,6 +10,50 @@ setInterval(() => {
   }
   const classNumber = allClass[classIndex];
   getClass.setAttribute("class", classNumber);
-  console.log(classNumber);
   classIndex++;
-}, 3000);
+}, 4000);
+
+// ---//
+const myslide = document.querySelectorAll(".trending-offer-details"),
+  dot = document.querySelectorAll(".dot");
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+  counter += 1;
+  slidefun(counter);
+}
+function plusSlides(n) {
+  counter += n;
+  slidefun(counter);
+  resetTimer();
+}
+function currentSlide(n) {
+  counter = n;
+  slidefun(counter);
+  resetTimer();
+}
+function resetTimer() {
+  clearInterval(timer);
+  timer = setInterval(autoSlide, 8000);
+}
+
+function slidefun(n) {
+  let i;
+  for (i = 0; i < myslide.length; i++) {
+    // myslide[i].style.display = "flex";
+    myslide[i].style.display = "none";
+  }
+  for (i = 0; i < dot.length; i++) {
+    dot[i].className = dot[i].className.replace(" active", "");
+  }
+  if (n > myslide.length) {
+    counter = 1;
+  }
+  if (n < 1) {
+    counter = myslide.length;
+  }
+  myslide[counter - 1].style.display = "block";
+  dot[counter - 1].className += " active";
+}
